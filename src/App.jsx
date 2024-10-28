e4c68af0eea999a4d1f4d285eb8ade3c7b9e0a65
Ajiboye Elijah <ajiboyeelijah242@gmail.com>
2024-10-28 20:41:44 +0100

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,28 @@ import divider from "./assets/images/pattern-divider-desktop.svg";
 const App = () => {
   const [advice, setAdvice] = useState({ id: null, advice: "" });
   const [loading, setLoading] = useState(true);
+  const [isGlowing, setIsGlowing] = useState(true);
 
   const fetchAdvice = () => {
     setLoading(true);
+    setIsGlowing(true);
     fetch('https://api.adviceslip.com/advice')
       .then(response => response.json())
       .then(data => {
         setAdvice(data.slip);
         setLoading(false);
+        setIsGlowing(false); // Turn off the glow effect once advice is fetched
       })
       .catch(error => {
         console.error('Error:', error);
         setLoading(false);
+        setIsGlowing(false);
       });
   };
 
   useEffect(() => {
     fetchAdvice();
   }, []);
-
-
 
   return (
     <div className="w-screen h-screen bg-DarkBlue flex items-center justify-center p-5 lg:p-0">
@@ -40,7 +42,11 @@ const App = () => {
 
         <img src={divider} alt="Divider" className="absolute bottom-24 md:w-[390px] w-[300px] h-[13px]" />
         
-        <div className="absolute bottom-0 translate-y-1/2 bg-Neon hover:shadow-[0px_0px_15px_3px_rgba(57,255,20,0.8)] p-5 rounded-full">
+        <div
+          className={`absolute bottom-0 translate-y-4 bg-Neon p-5 rounded-full ${
+            isGlowing ? 'hover:shadow-[0px_0px_15px_3px_rgba(57,255,20,0.8)]' : ''
+          }`}
+        >
           <img
             src={dice}
             alt="Dice Icon"
@@ -50,7 +56,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
